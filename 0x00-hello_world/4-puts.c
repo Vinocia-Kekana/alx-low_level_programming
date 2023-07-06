40f3914a6dee8851c8a9379746b5db31f73db2f8
vinocia-kekana <vinociakekana@gmail.com>
2023-07-06 17:19:46 +0200

    Add folder of folders

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A C Program that prints a line with puts function
+ *using the puts() function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
